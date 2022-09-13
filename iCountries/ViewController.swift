01ad10f044daf73d0b42b321f386257a7b2b74a3
shamrei12 <61946511+shamrei12@users.noreply.github.com>
2022-09-13 18:51:17 +0300

    Initial Commit

---
+++ iCountries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCountries
+//
+//  Created by Алексей Шамрей on 13.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
