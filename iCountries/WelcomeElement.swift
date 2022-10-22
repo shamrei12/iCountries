dd2117a3913d306b79631e8aab64310aa3dcbe85
shamrei12 <61946511+shamrei12@users.noreply.github.com>
2022-10-22 13:49:28 +0300

    added alamofire

--- iCountries/WelcomeElement.swift
+++ iCountries/WelcomeElement.swift
@@ -3,7 +3,17 @@
 //
 //   let welcome = try? newJSONDecoder().decode(Welcome.self, from: jsonData)
 
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseWelcomeElement { response in
+//     if let welcomeElement = response.result.value {
+//       ...
+//     }
+//   }
+
 import Foundation
+import Alamofire
 
 // MARK: - WelcomeElement
 struct WelcomeElement: Codable {
@@ -16,7 +26,7 @@ struct WelcomeElement: Codable {
     let independent: Bool?
     let status: Status
     let unMember: Bool
-    let currencies: [String: [String:String]]?
+    let currencies: [String:[String:String]]?
     let idd: Idd
     let capital: [String]?
     let altSpellings: [String]
@@ -26,6 +36,7 @@ struct WelcomeElement: Codable {
     let translations: [String: Translation]
     let latlng: [Double]
     let landlocked: Bool
+    let borders: [String]?
     let area: Double
     let demonyms: Demonyms?
     let flag: String
@@ -40,13 +51,30 @@ struct WelcomeElement: Codable {
     let startOfWeek: StartOfWeek
     let capitalInfo: CapitalInfo
     let postalCode: PostalCode?
-    let borders: [String]?
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseCapitalInfo { response in
+//     if let capitalInfo = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - CapitalInfo
 struct CapitalInfo: Codable {
     let latlng: [Double]?
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseCar { response in
+//     if let car = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - Car
 struct Car: Codable {
@@ -58,6 +86,15 @@ enum Side: String, Codable {
     case sideLeft = "left"
     case sideRight = "right"
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseCoatOfArms { response in
+//     if let coatOfArms = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - CoatOfArms
 struct CoatOfArms: Codable {
@@ -75,229 +112,264 @@ enum Continent: String, Codable {
     case southAmerica = "South America"
 }
 
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseCurrencies { response in
+//     if let currencies = response.result.value {
+//       ...
+//     }
+//   }
+
 // MARK: - Currencies
 struct Currencies: Codable {
-    let isk, eur, iqd, mwk: Aed?
-    let hkd, pyg, djf, mga: Aed?
-    let npr, dkk, nok, aud: Aed?
-    let tvd, lbp, xof, cve: Aed?
-    let gip, dzd, gel, mnt: Aed?
-    let irr, nzd, sgd, usd: Aed?
-    let nio, mdl, yer, gmd: Aed?
-    let kgs, cuc, cup, ern: Aed?
-    let krw, ghs, egp, xaf: Aed?
-    let twd, ckd, szl, zar: Aed?
-    let omr, uyu, bsd, brl: Aed?
-    let mmk, sek, vnd, awg: Aed?
-    let ils, jod, pen, xcd: Aed?
-    let bif, gtq, xpf, shp: Aed?
-    let lak, sll, inr, jmd: Aed?
-    let mkd, cad, lrd, bhd: Aed?
-    let bmd, myr, kid, uah: Aed?
-    let fok, afn, stn, kwd: Aed?
-    let pln, vuv, kmf, cny: Aed?
-    let mad, mru, pab, sbd: Aed?
-    let crc, pgk, jpy, ssp: Aed?
-    let gbp, ang, ves, thb: Aed?
-    let php, bnd, chf, cop: Aed?
-    let bzd, bob, rub, tzs: Aed?
-    let zwl, azn, mur, amd: Aed?
-    let czk, htg, lkr, all: Aed?
-    let syp, btn, jep, tnd: Aed?
-    let wst, currenciesTRY, aed: Aed?
-    let bam: BAM?
-    let bwp, ggp, scr, aoa: Aed?
-    let qar, nad, khr, gnf: Aed?
-    let uzs, zmw, sos, bgn: Aed?
-    let rwf, gyd, mxn, cdf: Aed?
-    let etb, mop, pkr, idr: Aed?
-    let ars, ron, ugx, srd: Aed?
-    let mvr, fjd, ttd, top: Aed?
-    let clp: Aed?
+    let bgn, fkp, syp, byn: Aed?
+    let inr, cuc, cup, eur: Aed?
+    let mkd, tjs, btn, xaf: Aed?
+    let sll, gbp, shp, uah: Aed?
+    let ils, sos, nio, lkr: Aed?
+    let usd, mdl, cad, aud: Aed?
+    let xof, twd, tnd, pgk: Aed?
+    let huf, dkk, xcd, xpf: Aed?
+    let hnl, fok, gnf, ang: Aed?
+    let kmf, hrk, cny, nzd: Aed?
+    let lrd, hkd, iqd, bwp: Aed?
+    let lyd, mnt, clp, zmw: Aed?
+    let chf, kgs, tmt, mur: Aed?
+    let php, bnd, sgd, kzt: Aed?
+    let sar, mxn, thb, irr: Aed?
+    let sbd, imp, wst, scr: Aed?
+    let rsd, gyd, mvr, mzn: Aed?
+    let awg, jep, sek, currenciesTRY: Aed?
+    let gel, yer, dop, uzs: Aed?
+    let kid, bbd, bmd, ugx: Aed?
+    let kes, bsd, crc, nok: Aed?
+    let vnd, kwd, mwk, bdt: Aed?
+    let lbp, kpw, ckd, kyd: Aed?
+    let rub, lak, tzs, ars: Aed?
     let sdg: BAM?
-    let lsl, fkp, mzn, tjs: Aed?
-    let kpw, rsd, hnl, kes: Aed?
-    let byn, dop, sar, bdt: Aed?
-    let bbd, imp, kzt, lyd: Aed?
-    let kyd, ngn, hrk, huf: Aed?
-    let tmt: Aed?
+    let tvd, npr: Aed?
+    let bam: BAM?
+    let pln, pyg, nad, zar: Aed?
+    let mru, szl, aed, cop: Aed?
+    let cve, gip, all, pen: Aed?
+    let ves, srd, jmd, dzd: Aed?
+    let top, brl, bhd, zwl: Aed?
+    let pkr, aoa, ngn, mop: Aed?
+    let afn, ssp, krw, qar: Aed?
+    let bzd, ttd, isk, rwf: Aed?
+    let azn, gmd, bif, ern: Aed?
+    let mad, ghs, ron, fjd: Aed?
+    let idr, jod, uyu, omr: Aed?
+    let bob, mga, vuv, lsl: Aed?
+    let ggp, khr, etb, stn: Aed?
+    let cdf, gtq, amd, jpy: Aed?
+    let djf, egp, myr, htg: Aed?
+    let czk, mmk, pab: Aed?
 
     enum CodingKeys: String, CodingKey {
-        case isk = "ISK"
-        case eur = "EUR"
-        case iqd = "IQD"
-        case mwk = "MWK"
-        case hkd = "HKD"
-        case pyg = "PYG"
-        case djf = "DJF"
-        case mga = "MGA"
-        case npr = "NPR"
-        case dkk = "DKK"
-        case nok = "NOK"
-        case aud = "AUD"
-        case tvd = "TVD"
-        case lbp = "LBP"
-        case xof = "XOF"
-        case cve = "CVE"
-        case gip = "GIP"
-        case dzd = "DZD"
-        case gel = "GEL"
-        case mnt = "MNT"
-        case irr = "IRR"
-        case nzd = "NZD"
-        case sgd = "SGD"
-        case usd = "USD"
-        case nio = "NIO"
-        case mdl = "MDL"
-        case yer = "YER"
-        case gmd = "GMD"
-        case kgs = "KGS"
+        case bgn = "BGN"
+        case fkp = "FKP"
+        case syp = "SYP"
+        case byn = "BYN"
+        case inr = "INR"
         case cuc = "CUC"
         case cup = "CUP"
-        case ern = "ERN"
-        case krw = "KRW"
-        case ghs = "GHS"
-        case egp = "EGP"
-        case xaf = "XAF"
-        case twd = "TWD"
-        case ckd = "CKD"
-        case szl = "SZL"
-        case zar = "ZAR"
-        case omr = "OMR"
-        case uyu = "UYU"
-        case bsd = "BSD"
-        case brl = "BRL"
-        case mmk = "MMK"
-        case sek = "SEK"
-        case vnd = "VND"
-        case awg = "AWG"
-        case ils = "ILS"
-        case jod = "JOD"
-        case pen = "PEN"
-        case xcd = "XCD"
-        case bif = "BIF"
-        case gtq = "GTQ"
-        case xpf = "XPF"
-        case shp = "SHP"
-        case lak = "LAK"
-        case sll = "SLL"
-        case inr = "INR"
-        case jmd = "JMD"
+        case eur = "EUR"
         case mkd = "MKD"
-        case cad = "CAD"
-        case lrd = "LRD"
-        case bhd = "BHD"
-        case bmd = "BMD"
-        case myr = "MYR"
-        case kid = "KID"
-        case uah = "UAH"
-        case fok = "FOK"
-        case afn = "AFN"
-        case stn = "STN"
-        case kwd = "KWD"
-        case pln = "PLN"
-        case vuv = "VUV"
-        case kmf = "KMF"
-        case cny = "CNY"
-        case mad = "MAD"
-        case mru = "MRU"
-        case pab = "PAB"
-        case sbd = "SBD"
-        case crc = "CRC"
-        case pgk = "PGK"
-        case jpy = "JPY"
-        case ssp = "SSP"
+        case tjs = "TJS"
+        case btn = "BTN"
+        case xaf = "XAF"
+        case sll = "SLL"
         case gbp = "GBP"
+        case shp = "SHP"
+        case uah = "UAH"
+        case ils = "ILS"
+        case sos = "SOS"
+        case nio = "NIO"
+        case lkr = "LKR"
+        case usd = "USD"
+        case mdl = "MDL"
+        case cad = "CAD"
+        case aud = "AUD"
+        case xof = "XOF"
+        case twd = "TWD"
+        case tnd = "TND"
+        case pgk = "PGK"
+        case huf = "HUF"
+        case dkk = "DKK"
+        case xcd = "XCD"
+        case xpf = "XPF"
+        case hnl = "HNL"
+        case fok = "FOK"
+        case gnf = "GNF"
         case ang = "ANG"
-        case ves = "VES"
-        case thb = "THB"
+        case kmf = "KMF"
+        case hrk = "HRK"
+        case cny = "CNY"
+        case nzd = "NZD"
+        case lrd = "LRD"
+        case hkd = "HKD"
+        case iqd = "IQD"
+        case bwp = "BWP"
+        case lyd = "LYD"
+        case mnt = "MNT"
+        case clp = "CLP"
+        case zmw = "ZMW"
+        case chf = "CHF"
+        case kgs = "KGS"
+        case tmt = "TMT"
+        case mur = "MUR"
         case php = "PHP"
         case bnd = "BND"
-        case chf = "CHF"
-        case cop = "COP"
-        case bzd = "BZD"
-        case bob = "BOB"
-        case rub = "RUB"
-        case tzs = "TZS"
-        case zwl = "ZWL"
-        case azn = "AZN"
-        case mur = "MUR"
-        case amd = "AMD"
-        case czk = "CZK"
-        case htg = "HTG"
-        case lkr = "LKR"
-        case all = "ALL"
-        case syp = "SYP"
-        case btn = "BTN"
-        case jep = "JEP"
-        case tnd = "TND"
-        case wst = "WST"
-        case currenciesTRY = "TRY"
-        case aed = "AED"
-        case bam = "BAM"
-        case bwp = "BWP"
-        case ggp = "GGP"
-        case scr = "SCR"
-        case aoa = "AOA"
-        case qar = "QAR"
-        case nad = "NAD"
-        case khr = "KHR"
-        case gnf = "GNF"
-        case uzs = "UZS"
-        case zmw = "ZMW"
-        case sos = "SOS"
-        case bgn = "BGN"
-        case rwf = "RWF"
-        case gyd = "GYD"
-        case mxn = "MXN"
-        case cdf = "CDF"
-        case etb = "ETB"
-        case mop = "MOP"
-        case pkr = "PKR"
-        case idr = "IDR"
-        case ars = "ARS"
-        case ron = "RON"
-        case ugx = "UGX"
-        case srd = "SRD"
-        case mvr = "MVR"
-        case fjd = "FJD"
-        case ttd = "TTD"
-        case top = "TOP"
-        case clp = "CLP"
-        case sdg = "SDG"
-        case lsl = "LSL"
-        case fkp = "FKP"
-        case mzn = "MZN"
-        case tjs = "TJS"
-        case kpw = "KPW"
-        case rsd = "RSD"
-        case hnl = "HNL"
-        case kes = "KES"
-        case byn = "BYN"
-        case dop = "DOP"
-        case sar = "SAR"
-        case bdt = "BDT"
-        case bbd = "BBD"
-        case imp = "IMP"
+        case sgd = "SGD"
         case kzt = "KZT"
-        case lyd = "LYD"
+        case sar = "SAR"
+        case mxn = "MXN"
+        case thb = "THB"
+        case irr = "IRR"
+        case sbd = "SBD"
+        case imp = "IMP"
+        case wst = "WST"
+        case scr = "SCR"
+        case rsd = "RSD"
+        case gyd = "GYD"
+        case mvr = "MVR"
+        case mzn = "MZN"
+        case awg = "AWG"
+        case jep = "JEP"
+        case sek = "SEK"
+        case currenciesTRY = "TRY"
+        case gel = "GEL"
+        case yer = "YER"
+        case dop = "DOP"
+        case uzs = "UZS"
+        case kid = "KID"
+        case bbd = "BBD"
+        case bmd = "BMD"
+        case ugx = "UGX"
+        case kes = "KES"
+        case bsd = "BSD"
+        case crc = "CRC"
+        case nok = "NOK"
+        case vnd = "VND"
+        case kwd = "KWD"
+        case mwk = "MWK"
+        case bdt = "BDT"
+        case lbp = "LBP"
+        case kpw = "KPW"
+        case ckd = "CKD"
         case kyd = "KYD"
+        case rub = "RUB"
+        case lak = "LAK"
+        case tzs = "TZS"
+        case ars = "ARS"
+        case sdg = "SDG"
+        case tvd = "TVD"
+        case npr = "NPR"
+        case bam = "BAM"
+        case pln = "PLN"
+        case pyg = "PYG"
+        case nad = "NAD"
+        case zar = "ZAR"
+        case mru = "MRU"
+        case szl = "SZL"
+        case aed = "AED"
+        case cop = "COP"
+        case cve = "CVE"
+        case gip = "GIP"
+        case all = "ALL"
+        case pen = "PEN"
+        case ves = "VES"
+        case srd = "SRD"
+        case jmd = "JMD"
+        case dzd = "DZD"
+        case top = "TOP"
+        case brl = "BRL"
+        case bhd = "BHD"
+        case zwl = "ZWL"
+        case pkr = "PKR"
+        case aoa = "AOA"
         case ngn = "NGN"
-        case hrk = "HRK"
-        case huf = "HUF"
-        case tmt = "TMT"
+        case mop = "MOP"
+        case afn = "AFN"
+        case ssp = "SSP"
+        case krw = "KRW"
+        case qar = "QAR"
+        case bzd = "BZD"
+        case ttd = "TTD"
+        case isk = "ISK"
+        case rwf = "RWF"
+        case azn = "AZN"
+        case gmd = "GMD"
+        case bif = "BIF"
+        case ern = "ERN"
+        case mad = "MAD"
+        case ghs = "GHS"
+        case ron = "RON"
+        case fjd = "FJD"
+        case idr = "IDR"
+        case jod = "JOD"
+        case uyu = "UYU"
+        case omr = "OMR"
+        case bob = "BOB"
+        case mga = "MGA"
+        case vuv = "VUV"
+        case lsl = "LSL"
+        case ggp = "GGP"
+        case khr = "KHR"
+        case etb = "ETB"
+        case stn = "STN"
+        case cdf = "CDF"
+        case gtq = "GTQ"
+        case amd = "AMD"
+        case jpy = "JPY"
+        case djf = "DJF"
+        case egp = "EGP"
+        case myr = "MYR"
+        case htg = "HTG"
+        case czk = "CZK"
+        case mmk = "MMK"
+        case pab = "PAB"
     }
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseAed { response in
+//     if let aed = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - Aed
 struct Aed: Codable {
     let name, symbol: String
 }
 
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseBAM { response in
+//     if let bAM = response.result.value {
+//       ...
+//     }
+//   }
+
 // MARK: - BAM
 struct BAM: Codable {
     let name: String
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseDemonyms { response in
+//     if let demonyms = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - Demonyms
 struct Demonyms: Codable {
@@ -305,10 +377,28 @@ struct Demonyms: Codable {
     let fra: Eng?
 }
 
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseEng { response in
+//     if let eng = response.result.value {
+//       ...
+//     }
+//   }
+
 // MARK: - Eng
 struct Eng: Codable {
     let f, m: String
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseIdd { response in
+//     if let idd = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - Idd
 struct Idd: Codable {
@@ -316,10 +406,28 @@ struct Idd: Codable {
     let suffixes: [String]?
 }
 
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseMaps { response in
+//     if let maps = response.result.value {
+//       ...
+//     }
+//   }
+
 // MARK: - Maps
 struct Maps: Codable {
     let googleMaps, openStreetMaps: String
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseName { response in
+//     if let name = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - Name
 struct Name: Codable {
@@ -327,10 +435,28 @@ struct Name: Codable {
     let nativeName: [String: Translation]?
 }
 
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseTranslation { response in
+//     if let translation = response.result.value {
+//       ...
+//     }
+//   }
+
 // MARK: - Translation
 struct Translation: Codable {
     let official, common: String
 }
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responsePostalCode { response in
+//     if let postalCode = response.result.value {
+//       ...
+//     }
+//   }
 
 // MARK: - PostalCode
 struct PostalCode: Codable {
@@ -359,3 +485,47 @@ enum Status: String, Codable {
 }
 
 typealias Welcome = [WelcomeElement]
+
+// MARK: - Helper functions for creating encoders and decoders
+
+func newJSONDecoder() -> JSONDecoder {
+    let decoder = JSONDecoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    return decoder
+}
+
+func newJSONEncoder() -> JSONEncoder {
+    let encoder = JSONEncoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        encoder.dateEncodingStrategy = .iso8601
+    }
+    return encoder
+}
+
+// MARK: - Alamofire response handlers
+
+extension DataRequest {
+    fileprivate func decodableResponseSerializer<T: Decodable>() -> DataResponseSerializer<T> {
+        return DataResponseSerializer { _, response, data, error in
+            guard error == nil else { return .failure(error!) }
+
+            guard let data = data else {
+                return .failure(AFError.responseSerializationFailed(reason: .inputDataNil))
+            }
+
+            return Result { try newJSONDecoder().decode(T.self, from: data) }
+        }
+    }
+
+    @discardableResult
+    fileprivate func responseDecodable<T: Decodable>(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
+        return response(queue: queue, responseSerializer: decodableResponseSerializer(), completionHandler: completionHandler)
+    }
+
+    @discardableResult
+    func responseWelcome(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<Welcome>) -> Void) -> Self {
+        return responseDecodable(queue: queue, completionHandler: completionHandler)
+    }
+}
